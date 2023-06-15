8289228fba3e151fe451a3c20b4cfefbea75d8e1
RenanSouza <renansouza0122@gmail.com>
2023-06-15 15:28:18 -0300

    erro message added

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
 const cardContainer = document.querySelector('.cards');
 const searchInput = document.querySelector('.input_container');
 const searchBtn = document.querySelector('.search_icon');
-const title = document.querySelector('.mainTitle');
 
 import { API_KEY } from "./config.js";
 
 searchInput.addEventListener('submit', searchMovie);
 searchBtn.addEventListener('click', searchMovie);
 
+
 async function searchMovie(e){
     const inputMovie = document.querySelector('.input_movie').value;
     const erroMessage  = document.querySelector('.erro');
-
     cardContainer.innerHTML = "";
     inputMovie === "" ? erroMessage.style.display = 'block' : erroMessage.style.display = 'none';
 
     e.preventDefault();
+
     const url = `https://api.themoviedb.org/3/search/movie?query=${inputMovie}&api_key=${API_KEY}`;
     const response = await fetch(url);
-    const data = await response.json();
-    const {results} =  data;
-    
-    results.forEach(result => {renderMovie(result)});
-    return data;
-
+    console.log(response);
+    console.log(response);
+        const data = await response.json();
+        console.log(data);
+        const {results} =  data;
+        results.forEach(result => {renderMovie(result)});
+        return data;
+  
 }
 
 async function getMovies(){
@@ -36,14 +38,18 @@ async function getMovies(){
 window.onload = async function(){
     const movies = await getMovies();
     const {results} = movies;
-    
     results.forEach(result =>{renderMovie(result)})
 }
 
 function renderMovie(movie){
     const {title, poster_path, popularity} = movie;
-    const imageMovie = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+    let  imageMovie = `https://image.tmdb.org/t/p/w500/${poster_path}`;
+
+    if(!poster_path){
+         imageMovie = 'https://img.freepik.com/free-vector/404-error-with-cone-signal_24908-77776.jpg?w=740&t=st=1686852991~exp=1686853591~hmac=7c24e1cd1d058b65a2c64c3641ac572fa2681b877334fe5ee89b2276128dfdbd';
+    }
     
+
     cardContainer.innerHTML += `
     <div class="card">
         <img class="image_card" src="${imageMovie}" alt="image-movie">
