a754977e35b6706ce02ad06488014d2d7e523537
RenanSouza <renansouza0122@gmail.com>
2023-06-27 18:39:13 -0300

    responsivity updated

--- js/main.js
+++ js/main.js
@@ -58,12 +58,6 @@ function renderMovie(movie){
 
     cardContainer.innerHTML += `
     <div class="card">
-        <div class="icon_closeCard">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-      </svg>
-      
-        </div>
         <img class="image_card" src="${imageMovie}" alt="image-movie" data-atropos-offset="5">
         <div class="info">
             <h2 data-atropos-offset="5">${title}</h2>
@@ -75,8 +69,8 @@ function renderMovie(movie){
     `
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.addEventListener('click',cardInformation));
-  
-}
+ }
+
 
 function closeCardInfo(e){
     
